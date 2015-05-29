d7e454b637c84fbd3bf7bf2a64585b660539cb53
alinc <a@a.com>
2015-05-29 23:57:12 +0200

    first commit

---
+++ app/app.js
@@ -0,0 +1,114 @@
+angular.module('app', [])
+
+.controller('TestRendering', ['$rootScope', '$location', '$scope', 'TestService', function($rootScope, $location, $scope, TestService){
+
+	$rootScope.$on('renderComplete', function(e, d){
+		$scope.duration = d;		
+		$scope.totalWatchers = TestService.countWatchers();
+	});
+	
+	$scope.run500 = function(){
+		$scope.run(500);
+	};
+	
+	$scope.run1500 = function(){
+		$scope.run(1500);
+	};
+	
+	$scope.run2500 = function(){
+		$scope.run(2500);
+	};
+	
+	$scope.run5000 = function(){
+		$scope.run(5000);
+	};
+	
+	$scope.run = function(counter){
+		$scope.running = true;
+		TestService.load(counter)
+			.success(function(response){
+				$scope.dataset = response;
+				$scope.running = false;
+			})
+			.error(function(e){
+				$scope.running = false;
+				console.log(e);
+			});
+	}
+	
+	$scope.isActive = function(route) {
+        return $location.absUrl().endsWith(route);
+    }
+
+}])
+
+.service('TestService', ['$http', function($http){
+
+	var startTime;
+	
+	this.getStart = function(){
+		return startTime;
+	}
+	
+	this.setStart = function(){
+		startTime = new Date();
+	}
+	
+	this.load = function(recCount)
+	{	
+		this.setStart();
+		
+		return $http(
+		{
+			url: 'data' + recCount + '.json',
+			params: { 'nocache': new Date().getTime() }
+		});
+	}
+	
+	this.countWatchers = function(){
+		var root = angular.element(document.getElementsByClassName('table'));
+		var watchers = 0;
+
+		var f = function (element) {
+			if (element.data().hasOwnProperty('$scope')) {
+				watchers += (element.data().$scope.$$watchers || []).length;
+			}
+
+			angular.forEach(element.children(), function (childElement) {
+				f(angular.element(childElement));
+			});
+		};
+
+		f(root);
+
+		return watchers;
+	}
+}])
+
+.filter('cf', function(){
+	return function(input)
+	{
+		return input;
+	};
+})
+
+// http://tech.small-improvements.com/2013/09/10/angularjs-performance-with-large-lists/
+.directive('postRepeatDirective', 
+  ['$rootScope', '$timeout', '$log',  'TestService', 
+  function($rootScope, $timeout, $log, TestService) {
+    return function(scope, element, attrs) {
+      if (scope.$last){
+         $timeout(function(){
+             var ref = new Date(TestService.getStart());
+             var end = new Date();
+			 var duration = end-ref;
+			 
+             $log.debug("## DOM rendering list took: " + (end - ref) + " ms");
+			 
+			 $rootScope.$emit('renderComplete', duration);
+         });
+       }
+    };
+  }
+]);
+
---
+++ data-gen.js
@@ -0,0 +1,97 @@
+var jsf = require('json-schema-faker');
+var fs = require('fs');
+
+//https://github.com/marak/Faker.js/
+var schema = {
+  type: 'object',
+  properties: {
+	id: {
+	  $ref: '#/definitions/positiveInt'
+	},
+	status: {
+	  type: 'boolean',
+	  faker: "true|false"
+	},
+	name: {
+	  type: 'string',
+	  faker: 'name.findName'
+	},
+	email: {
+	  type: 'string',
+	  format: 'email',
+	  faker: 'internet.email'
+	},
+	 field1: {
+		type: 'string',
+		minLength: 5,
+		maxLength: 25
+	},
+	field2: {
+		type: 'string',
+		minLength: 5,
+		maxLength: 25
+	},
+	field3: {
+		type: "string",
+		pattern: "yes|no|maybe|i don't know"
+	},
+	field4: {
+		type: "string",
+		faker: 'address.city'
+	},
+	field5: {
+		type: "string",
+		faker: 'address.zipCode'
+	},
+	field6Date: {
+		type: "number",
+		faker: 'date.past'
+	},
+	field7: {
+		type: "string",
+		faker: 'phone.phoneNumber'
+	},
+	field8: {
+		type: "string",
+		faker: 'internet.userName'
+	},
+	field9: {
+		type: "string",
+		faker: 'company.companyName'
+	},
+	field10: {
+		type: "string",
+		faker: 'image.imageUrl'
+	}
+  },
+  required: ['id', 'name', 'email', 'status', 'field6Date', 'field1', 'field2', 'field3', 'field4', 'field5', 'field7', 'field8', 'field9', 'field10'],
+  definitions: {
+    positiveInt: {
+      type: 'integer',
+      minimum: 0,
+      minimumExclusive: true
+    }
+  }
+};
+
+var totalRecordsToFake = 500;
+if(process.argv.length==3)
+{
+	totalRecordsToFake = process.argv[2];
+}
+
+var datafile = "data" + totalRecordsToFake + ".json";
+
+var result = [];
+for(i=0;i<totalRecordsToFake;i++)
+{
+	result.push(jsf(schema));
+}
+
+fs.writeFile(datafile, JSON.stringify(result, null, 2), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("Generated " + totalRecordsToFake + " records");
+}); 
